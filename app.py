13a4c1114bda7af2d089102ef7877fb80f0b3b90
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-08-18 18:55:51 -0700

    [ADD] Created main file

---
+++ app.py
@@ -0,0 +1,8 @@
+"""
+PROJECT:        Basic Weather App
+FILENAME:       app.py
+AUTHOR:         Aakash Sudhakar
+DESCRIPTION:    Basic take home project for SPD 1.02. Construct
+                a functional weather app with good code practices.
+"""
+
