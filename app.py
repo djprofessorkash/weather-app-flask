b7488d2f542ad0f5a7a450b417d42757aafb581b
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-08-18 19:00:37 -0700

    [EDIT] Create index route with basic route call

--- app.py
+++ app.py
@@ -6,3 +6,32 @@ DESCRIPTION:    Basic take home project for SPD 1.02. Construct
                 a functional weather app with good code practices.
 """
 
+############################################################
+################ IMPORTS AND INITIALIZATIONS ###############
+############################################################
+
+
+from flask import Flask, render_template
+import requests, json
+
+app = Flask(__name__)
+
+
+############################################################
+######################## APP ROUTING #######################
+############################################################
+
+
+@app.route("/")
+def get_index():
+    pass
+
+
+############################################################
+######################## APP ROUTING #######################
+############################################################
+
+
+if __name__ == "__main__":
+    HOSTNAME, PORTNAME = "0.0.0.0", "8080"
+    app.run(host=HOSTNAME, port=PORTNAME, debug=True)
