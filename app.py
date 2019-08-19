2e096d8fa2f0983725dd1ae40eeabef11992e975
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-08-18 19:09:39 -0700

    [EDIT] Created Basic Functional App (Needs HTML)

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ DESCRIPTION:    Basic take home project for SPD 1.02. Construct
 
 
 from flask import Flask, render_template
-import requests, json
+import requests as req
+import json
 
 app = Flask(__name__)
 
@@ -24,7 +25,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def get_index():
-    pass
+    # Instantiate Weather App Template
+    PATH_INDEX = "index.html"
+
+    # Instantiate Weather API Location
+    URL = "https://api.openweathermap.org/data/2.5/weather?lat=37.7749&lon=122.4194&units=metric&APPID=a4f15e7f800810b40af6bbc478d3e9c5"
+
+    # Convert Weather Call to Parseable JSON Object
+    parseable = req.get(URL).content
+    data = json.loads(parseable)
+
+    # Extract and Return Data from Parsed Weather JSON Object
+    weather_all = data["weather"]
+    weather_main, weather_desc = weather_all[0]["main"], weather_all[0]["description"]
+    return render_template(PATH_INDEX, weather=weather_main, description=weather_desc)
 
 
 ############################################################
@@ -33,5 +47,5 @@ def get_index():
 
 
 if __name__ == "__main__":
-    HOSTNAME, PORTNAME = "0.0.0.0", "8080"
-    app.run(host=HOSTNAME, port=PORTNAME, debug=True)
+    hostname, portname = "0.0.0.0", "8080"
+    app.run(host=hostname, port=portname, debug=True)
